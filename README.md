6b387d387caeeceb4f25245463426bba2330a019
sahasatvik <sahasatvik@gmail.com>
2016-03-31 20:25:47 +0530

    Added extra features to Sieve.java

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ A collection of simple programs which relate to prime nummbers !
 
 Programs in this repository :
 
-| Program Name  | Purpose                          | Usage                                  |
-| --------------|----------------------------------|----------------------------------------|
-| Sieve         |   Compute and display all prime  | `Sieve [maxRange - 100 by default]`    |
-|               | numbers below a specified number |                                        |
+| Program Name  | Purpose                                               | Usage                                  |
+| :------------:|-------------------------------------------------------|:--------------------------------------:|
+| Sieve         | Display prime numbers using the Sieve of Eratosthenes | `java Sieve [-h] [-c | -C] maxNumber`  |
+
 
--- Sieve.java
+++ Sieve.java
@@ -1,6 +1,9 @@
+import java.text.NumberFormat;
+
 class Sieve {
 	
 	int size;
+	int primeCount;
 	boolean[] sieve;
 		
 	Sieve (int size) {
@@ -25,20 +28,84 @@ class Sieve {
 			while (!sieve[++p]); 
 		}
 	}
-	void displayPrimes () {
-		int primeCount = 0;
+	void displayPrimes (boolean count, boolean countOnly) {
+		primeCount = 0;
 		for (int i = 2; i <= size; i++) {
 			if (sieve[i]) {
 				primeCount++;
-				System.out.printf("%d ", i);
+				if (!countOnly) { 
+					System.out.printf("%d ", i);
+				}
 			}
 		}
-		System.out.printf("%nPrime Count : %d%n", primeCount);
+		if (countOnly) {
+			System.out.printf("%d", primeCount);
+		} else if (count) {
+			System.out.printf("%n");
+			System.out.printf("%nPrime Count : %d%n", primeCount);
+		} else {
+			System.out.printf("%n");
+		
+		}
+	}
+	
+	static int argBase;
+	static int numberOfArgs;
+	static boolean help;
+	static boolean count;
+	static boolean countOnly;
+
+	public static boolean hasMoreArgs () {
+	    return numberOfArgs > argBase;
+	}
+	
+	public static void displayHelp () {
+		System.out.println("\nSieve - a simple program to display primes less than 'maxNumber' using the sieve of Eratosthenes.");
+		System.out.println("Usage :");
+		System.out.println("\tjava Sieve [-h] [-c | -C] maxNumber\n");
+		System.out.println("\t\t-h or --help        :   Displays this text");
+		System.out.println("\t\t-c or --count       :   Displays number of primes detected");
+		System.out.println("\t\t-C or --count-only  :   Only outputs number of primes detected");
+		System.out.println("\t\tmaxnumber           :   Number below which primes are to be found (exclusive of maxNumber)\n");
 	}
 
 	public static void main (String[] args) {
-		int size = (args.length > 0)? Integer.parseInt(args[0]) : 100;
-		Sieve eratosthenes = new Sieve(size);
-		eratosthenes.displayPrimes();
+	
+		argBase = 0;
+		numberOfArgs = args.length;
+		
+		try {
+			help = !hasMoreArgs() || args[argBase].equals("-h") || args[argBase].equals("--help");
+			if (help) {
+			    displayHelp();
+				argBase++;
+			}
+			if (hasMoreArgs()) {
+				count = args[argBase].equals("-c") || args[argBase].equals("--count");
+				if (count) {
+					argBase++;
+				} else {
+				    countOnly = args[argBase].equals("-C") || args[argBase].equals("--count-only");
+				    if (countOnly) {
+   					    argBase++;
+   				    }
+				}
+				if (hasMoreArgs()) {
+					int size = Integer.parseInt(args[argBase]);
+					if (size > 1) {
+						Sieve eratosthenes = new Sieve(size);
+						eratosthenes.displayPrimes(count, countOnly);
+					} else {
+						System.out.println("maxNumber is too less ! Try keeping maxNumber above 1");
+					}
+				} else {
+					System.out.println("Too few arguments !");
+				}
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("Incorrect usage ! Type in    java Sieve -h    to see correct usage");
+		} catch (Exception e) {
+			System.out.println("Exception !" + e.getMessage());
+		}
 	}
 }
